fa2974dfc6172380286911574c9768233eb12adf
Hartmut Holzgraefe <hholzgra@php.net>
2006-07-29 00:59:21 +0000

    fixed GPL parse error, included GPL in package file list
    git-svn-id: http://svn.php.net/repository/pear/packages/CodeGen/trunk@217243 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeGen/License/GPL.php
+++ CodeGen/License/GPL.php
@@ -61,8 +61,8 @@ class CodeGen_License_GPL extends CodeGen_License
      */
     function getComment() 
     {
-        return <<<EOD   
-   | This program is free software; you can redistribute it and/or        |
+      return  
+"   | This program is free software; you can redistribute it and/or        |
    | modify it under the terms of the GNU General Public License          |
    | as published by the Free Software Foundation; either version 2       |                 
    | of the License, or (at your option) any later version.               | 
@@ -79,7 +79,7 @@ class CodeGen_License_GPL extends CodeGen_License
    |   Free Software Foundation, Inc.,                                    |
    |   59 Temple Place, Suite 330,                                        |
    |   Boston, MA  02111-1307  USA                                        |
-EOD;
+";
     }
 
     /**
